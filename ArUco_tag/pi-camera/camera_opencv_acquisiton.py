b070ca48fe50e7de1dc2d5a59b0e1ea77c110804
Gunlek <fabien.aubret@gmail.com>
2019-11-02 08:31:30 +0000

    [Update] Updated script to customize filename using parameters

--- ArUco_tag/pi-camera/camera_opencv_acquisiton.py
+++ ArUco_tag/pi-camera/camera_opencv_acquisiton.py
@@ -2,6 +2,11 @@ import cv2 as cv
 from picamera import PiCamera
 from picamera.array import PiRGBArray
 import time
+import sys
+
+filename = "opencv_capture.jpg"
+#if len(sys.argv) > 0:
+#	filename = sys.argv[0]
 
 camera = PiCamera()
 rawCapture = PiRGBArray(camera) #RGB Array allows image usage without jpeg compression => Faster
@@ -12,6 +17,6 @@ time.sleep(2)
 camera.capture(rawCapture, format="bgr")
 image = rawCapture.array
 
-cv.imwrite("opencv_capture.jpg", image)
+cv.imwrite(filename, image)
 # cv.imshow("Image", image)
 # cv.waitKey(0)
